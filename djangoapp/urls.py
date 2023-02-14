115507736ea2e728194f85e7404f587af545ea40
Derya Cortuk <53267226+deryacortuk@users.noreply.github.com>
2023-02-14 12:16:38 +0300

    Update urls.py

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -18,8 +18,7 @@ from django.urls import path,include
 from article import views
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 
 urlpatterns = [
